d12ee8fd0a5f165c4328688cfa5e0858b27b65a4
larafaelivrin <lara.faelivrin@gmail.com>
2017-01-05 15:26:56 +0000

    rename

---
+++ charts/templates/5040.js
@@ -0,0 +1,70 @@
+var chartOptions = {
+  "xAxis": {
+    "tickInterval": 1
+  },
+  "yAxis": {
+    "min": -20, 
+	"labels": {
+		"format": "{value}%"
+	}
+  },	
+  "tooltip": {
+    "shared": false, 
+	"pointFormat": '<span style="color:{series.color}">\u25CF</span> {series.name}: <b>{point.y}%</b><br/>'
+  },
+  "series": [
+	  {"color": "#8a8a8a",
+	   "index": 2,
+	   "visible": true,
+	     "marker": {
+        "enabled": true
+    } 
+	  }, /* BS = grau */
+    {"color": "#71a3b5",
+    "visible": false,
+       "index": 4
+    }, /* ZH = hellblau*/
+	  {"color": "#933f8d",
+	  "visible": false,
+	     "index":5
+	  }, /* GE = violett*/
+    {"color": "#ff8028",
+    "visible": false,
+       "index": 3
+    }, /* BL = orange*/	
+  	{"color": "#256370",
+  	"visible": false,
+  	   "index": 6
+  	}, /* AG = dunkelblau */
+  	{"color": "#b00000",
+  	"visible": true,
+  	 "index": 1,
+  	    "marker": {
+        "enabled": true
+    }
+  	}  /* CH = rot */
+  ],
+  "legend": {
+    "enabled": true,
+    "x": 40,
+    "y": 35,
+     "itemWidth": 210,
+    "layout": "horizontal",
+    "verticalAlign": "top",
+    "itemMarginBottom": 5,
+    "align": "left",
+    "itemStyle": {
+      "fontWeight": "normal"
+    }
+  },
+  "plotOptions": {
+    "line": {
+      "marker":{
+        "enabled": false,
+        "symbol": "circle",
+      } 
+    }
+  }
+};
+ 
+ 
