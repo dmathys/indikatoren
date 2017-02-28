9459e7c4a56383e850d5f73ebe6b155d43741544
grilli73 <nathalie.grillon@gmail.com>
2017-02-28 10:47:48 +0000

    neuer chart

--- charts/templates/2173.js
+++ charts/templates/2173.js
@@ -6,25 +6,20 @@
     }
   },
   "xAxis": {
-    "tickInterval": 4
+    "tickInterval": 8
   },
   "tooltip": {
     "shared": false
   },
   "series": [
-    {
-      "color": "#69929B",
-      "marker": {
-        "enabled": true
-      }
-  },
+    {"color": "#69929B", "lineWidth": 5},
     {"color": "#D3E2E5"},
     {"color": "#9F7C5A"},
     {"color": "#E7CFE2"}
   ],
   "legend": {
     "enabled": true,
-    "x": 20,
+    "x": 45,
     "y": 35,
     "layout": "horizontal",
     "verticalAlign": "top",
@@ -33,6 +28,14 @@
     "itemStyle": {
       "fontWeight": "normal"
     }
+  },
+  "plotOptions": {
+    "line": {
+      "marker":{
+        "enabled": false,
+        "symbol": "circle",
+      } 
+    },
   },
 	}
 }());
